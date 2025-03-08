2a7645255ab9e5bf9384b2297740b1dbca0b5bd7
Myrork <ilya.127000@yandex.ru>
2025-03-08 23:17:45 +0300

    11-04

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
@@ -11,5 +11,5 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume('hello', callback, auto_ack=True)
 channel.start_consuming()
